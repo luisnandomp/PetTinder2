7863e5040bdf07d3078584eea345afdf73c71bb4
marialuizacamilo92@gmail.com <marialuizacamilo92@gmail.com>
2023-04-24 21:01:55 -0300

    paginafiltro

---
+++ resources/views/filtroanimal/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtro do animal </title>
+</head>
+<body>
+<select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+</body>
+</html>
