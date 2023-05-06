4ad86b1a24d7d3cc8bb12c2f39d89f0970d26acf
Larissa Justi <larissacalcanhojusti@gmail.com>
2023-05-05 22:50:21 -0300

    somos quem

--- resources/views/sites/quem.blade.php
+++ resources/views/sites/quem.blade.php
@@ -2,7 +2,8 @@
 
 @section('conteudo')
 
-    <img src="{{ asset('imagens/lindos.png') }}" alt="...">
-    <h1>Este projeto tem como objetivo auxiliar os donos de pets a encontrar seus amigos perdido e também ajudar outros animais a encontrar um lar. Desenvolvendo um site para facilitar a essa busca na cidade de Guararapes.</h1>
+    <img src="{{ asset('imagens/lindos.jpeg') }}" class="lindos"alt="...">
+    <h1 class="objetivo">O aumento de casos de animais perdidos no município de Guararapes está aumentando a cada dia mais, com isso consequentemente os pets acabam ficando sem lar. Com a aplicação (sistema PET) os usuários têm a possibilidade de encontrar seu pet de forma mais fácil. Além disso existe muitos animais domésticos que estão abandonados nas ruas, que precisam de lar. O sistema viabiliza o auxílio de encontrar uma morada para o pet expondo todas as suas condições.
+        Com isso o objetivo do sistema é gerenciar e facilitar a adoção e a encontrar seu animal, seja resgatando ou pela adoção responsável. O Sistema possui a opção da interação entre os usuários fazendo postagens e por publicações, com o intuito de facilitar a comunicação entre os usuários. </h1>
 
 @endsection
