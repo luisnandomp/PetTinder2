2a0f6487c8723504423a4fbbfdb709e4e1d8705c
luisnandomp <luisnando.marques2107@gmail.com>
2023-05-05 21:55:30 -0300

    agora foi

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends Controller
 
     public function quem()
     {
-        return view('sites.patrocinio');
+        return view('sites.quem');
     }
 
     public function patrocinio()
