b2943b53841750cefc512cf86c80dacae1296f2c
Larissa Justi <larissacalcanhojusti@gmail.com>
2023-05-05 22:02:24 -0300

    quem somos

--- resources/views/sites/quem.blade.php
+++ resources/views/sites/quem.blade.php
@@ -2,5 +2,7 @@
 
 @section('conteudo')
 
+    <img src="{{ asset('imagens/lindos.png') }}" alt="...">
+    <h1>Este projeto tem como objetivo auxiliar os donos de pets a encontrar seus amigos perdido e também ajudar outros animais a encontrar um lar. Desenvolvendo um site para facilitar a essa busca na cidade de Guararapes.</h1>
 
 @endsection
