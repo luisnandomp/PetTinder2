a67c0f09f5df8c17356f21b26f5acd5b0793508c
larissa.justi <larissacalcanhojusti>
2023-05-09 22:34:46 -0300

    desing quem

--- resources/views/publicacoes/busca.blade.php
+++ resources/views/publicacoes/busca.blade.php
@@ -53,10 +53,36 @@
     </form>
 
     @foreach ($publicacoes as $publicacao)
-        <div class="border p-4 rounded">
+       <!-- <div class="border p-4 rounded">
            <h1>{{ $publicacao->animal->apelido }}</h1>
            <h1>{{ $publicacao->usuario->primeiro_nome }}</h1>
         </div>
+    -->
+
+
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $publicacao->animal->apelido }}</h5>
+                      <p class="card-text">{{ $publicacao->usuario->primeiro_nome }}</p>
+                      <a href="#" class="btn btn-primary">Meu Perfil</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $publicacao->animal->apelido }}</h5>
+                      <p class="card-text">{{ $publicacao->usuario->primeiro_nome }}</p>
+                      <a href="#" class="btn btn-primary">Meu Perfil</a>
+                    </div>
+                  </div>
+            </div>
+          </div>
+
     @endforeach
 
     {{ $publicacoes->links() }}
--- resources/views/sites/quem.blade.php
+++ resources/views/sites/quem.blade.php
@@ -2,8 +2,13 @@
 
 @section('conteudo')
 
-    <img src="{{ asset('imagens/lindos.jpeg') }}" class="lindos"alt="...">
-    <h1 class="objetivo">O aumento de casos de animais perdidos no município de Guararapes está aumentando a cada dia mais, com isso consequentemente os pets acabam ficando sem lar. Com a aplicação (sistema PET) os usuários têm a possibilidade de encontrar seu pet de forma mais fácil. Além disso existe muitos animais domésticos que estão abandonados nas ruas, que precisam de lar. O sistema viabiliza o auxílio de encontrar uma morada para o pet expondo todas as suas condições.
-        Com isso o objetivo do sistema é gerenciar e facilitar a adoção e a encontrar seu animal, seja resgatando ou pela adoção responsável. O Sistema possui a opção da interação entre os usuários fazendo postagens e por publicações, com o intuito de facilitar a comunicação entre os usuários. </h1>
+    <div class="row">
+        <img src="{{ asset('imagens/lindos.jpeg') }}" class="lindos" alt="...">
+        <div class="col">
+            <h1> Quem Somos </h1>
+            <h2 class="objetivo">Somos alunos da Etec e cursamos Desenvolvimento de Sistemas, decidimos criar este projeto como a nossa conclusão deste curso. A ideia surgiu, pois com o aumento de casos de animais perdidos no município de Guararapes está aumentando a cada dia mais, com isso consequentemente os pets acabam ficando sem lar. Com a aplicação (sistema PET) os usuários têm a possibilidade de encontrar seu pet de forma mais fácil. Além disso existe muitos animais domésticos que estão abandonados nas ruas, que precisam de lar. O sistema viabiliza o auxílio de encontrar uma morada para o pet expondo todas as suas condições.
+                Com isso o objetivo do sistema é gerenciar e facilitar a adoção e a encontrar seu animal, seja resgatando ou pela adoção responsável. O Sistema possui a opção da interação entre os usuários fazendo postagens e por publicações, com o intuito de facilitar a comunicação entre os usuários. </h2>
+        </div>
+    </div>
 
 @endsection
