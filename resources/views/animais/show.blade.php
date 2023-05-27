b995271ff2ff3cc657094cfae650f5705568106b
larissa.justi <larissacalcanhojusti@gmail.com>
2023-05-26 21:55:13 -0300

    marciano

--- resources/views/animais/show.blade.php
+++ resources/views/animais/show.blade.php
@@ -5,37 +5,37 @@
 
 <h1>Perfil de seus animais cadastrados</h1><br> <br>
 
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-        <strong></strong><img width="100px" src="/storage/{{ $animal->foto }}" alt="" class="imagem">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text"><ul>
-            <ul class="list-group">
-                <li class="list-group-item"><strong>Raça: </strong> {{$animal->raca}} </li>
-                <li class="list-group-item"><strong>Porte: </strong>{{$animal->porte}}</li>
-                <li class="list-group-item"><strong>Idade: </strong>{{$animal->idade}}</li>
-                <li class="list-group-item"><strong>Cor: </strong>{{$animal->cor}}</li>
-                <li class="list-group-item"><strong>Personalidade: </strong>{{$animal->personalidade}}</li>
-                <li class="list-group-item"><strong>Comorbidade: </strong>{{$animal->comorbidade}} </li>
-                <li class="list-group-item"><strong>Gênero: </strong>{{$animal->genero}}</li>
-                <li class="list-group-item"><strong>Apelido: </strong>{{$animal->apelido}}</li>
-                <li class="list-group-item"><strong>Vacina: </strong>{{$animal->vacina}}</li>
-                <li class="list-group-item"><strong>Castração: </strong>{{$animal->castracao}}</li>
-                <li class="list-group-item"><strong>Localidade: </strong>{{$animal->localidade}}</li>
-                <li class="list-group-item"><strong>Observação: </strong>{{$animal->observacao}}</li>
-              </ul>
-            </ul></small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
+            <div class="card mb-3 center">
+                <div class="row g-0">
+                <div class="col-md-4">
+                <strong></strong><img width="100px" src="/storage/{{ $animal->foto }}" alt="" class="imagem">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                    <h5 class="card-title" class="fa fa-align-center" aria-hidden="true">{{$animal->apelido }}</h5>
+                    <p class="card-text"><ul>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Raça: </strong> {{$animal->raca}} </li>
+                            <li class="list-group-item"><strong>Porte: </strong>{{$animal->porte}}</li>
+                            <li class="list-group-item"><strong>Idade: </strong>{{$animal->idade}}</li>
+                            <li class="list-group-item"><strong>Cor: </strong>{{$animal->cor}}</li>
+                            <li class="list-group-item"><strong>Personalidade: </strong>{{$animal->personalidade}}</li>
+                            <li class="list-group-item"><strong>Comorbidade: </strong>{{$animal->comorbidade}} </li>
+                            <li class="list-group-item"><strong>Gênero: </strong>{{$animal->genero}}</li>
+                            <li class="list-group-item"><strong>Apelido: </strong>{{$animal->apelido}}</li>
+                            <li class="list-group-item"><strong>Vacina: </strong>{{$animal->vacina}}</li>
+                            <li class="list-group-item"><strong>Castração: </strong>{{$animal->castracao}}</li>
+                            <li class="list-group-item"><strong>Localidade: </strong>{{$animal->localidade}}</li>
+                            <li class="list-group-item"><strong>Observação: </strong>{{$animal->observacao}}</li>
+                          </ul>
+                        </ul></small></p>
+                    </div>
+                </div>
+                </div>
           <button type="button" class="btn btn-success"><a href="{{route('animais.index')}}">voltar para lista de Animais</a></button>
           <button type="button" class="btn btn-success"><a href="{{route('animais.create')}}">Cadastrar outro animal</a></button>
+            </div>
+
             <style>
                 .imagem{
                     height: 60%;
@@ -47,4 +47,6 @@
                     padding-right: 25px;
                 }
             </style>
+
 @endsection
+
