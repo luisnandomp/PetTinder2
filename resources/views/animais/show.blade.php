0ddefeffcf16b5349baf2e57cbe1853e6546d2d5
larissa Justi <larissacalcanhojusti@gmail.com>
2023-05-31 22:41:11 -0300

    descrição

--- resources/views/animais/show.blade.php
+++ resources/views/animais/show.blade.php
@@ -12,104 +12,103 @@
                 <h5 class="perfil-titulo" class="fa fa-align-center" aria-hidden="true">{{$animal->apelido }}</h5>
                 <p class="card-text">
 
-
                 <div class="container text-center">
                     <div class="row">
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Espécie</div>
-                                    {{$animal->pet}}
+                                    <p class="descricao-perfil">{{$animal->pet}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Raça</div>
-                                    {{$animal->raca}}
+                                    <p class="descricao-perfil">{{$animal->raca}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
-                                    <div class="">Sexo</div>
-                                    {{$animal->genero}}
+                                    <div class="fw-bold">Sexo</div>
+                                    <p class="descricao-perfil">{{$animal->genero}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6"">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Porte</div>
-                                    {{$animal->porte}}
+                                    <p class="descricao-perfil">{{$animal->porte}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Idade</div>
-                                    {{$animal->idade}}
+                                    <p class="descricao-perfil">{{$animal->idade}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Cor</div>
-                                    {{$animal->cor}}
+                                    <p class="descricao-perfil"> {{$animal->cor}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Personalidade</div>
-                                    {{$animal->personalidade}}
+                                    <p class="descricao-perfil">{{$animal->personalidade}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-5">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Comorbidade</div>
-                                    {{$animal->comorbidade}}
+                                    <p class="descricao-perfil">{{$animal->comorbidade}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-5">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Vacina</div>
-                                    {{$animal->vacina}}
+                                    <p class="descricao-perfil">{{$animal->vacina}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Castração</div>
-                                    {{$animal->castracao}}
+                                    <p class="descricao-perfil">{{$animal->castracao}}</p>
                                 </div>
                             </li>
                         </div>
                     
-                        <div class="col-6 col-sm-4">
+                        <div class="col-12 col-sm-12 col-md-6">
                             <li class="list-group-item d-flex justify-content-between align-items-start">
                                 <div class="ms-2 me-auto">
                                     <div class="fw-bold">Localização</div>
-                                    {{$animal->localidade}}
+                                    <p class="descricao-perfil">{{$animal->localidade}}</p>
                                 </div>
                             </li>
                         </div>
