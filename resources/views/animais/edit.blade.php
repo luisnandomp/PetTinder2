2aa79da4f73878fcf596ad8b017550bb78d4cfa0
larissa justi <larissacalcanhojusti@gmail.com>
2023-06-12 22:15:01 -0300

    edição pet..

--- resources/views/animais/edit.blade.php
+++ resources/views/animais/edit.blade.php
@@ -3,8 +3,6 @@
 @section('conteudo')
 
 @vite(['resources/scss/publicacao.scss'])
-<div class="editar">
-
         <form action="{{ route('animais.update', $animal->id) }}" method="POST" class="editar" enctype="multipart/form-data">
             <h1 aling="center" class="titulo-edit">{{ $animal->apelido }}</h1>
 
@@ -23,8 +21,12 @@
                 </div>
                 <div class="col">
                     <label class="campo-animal">Porte</label>
-                    <input type="text" class="form-control" value="{{ $animal->porte }}">
-                </div>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" value="{{ $animal->porte }}">
+                    <option selected>Grande</option>
+                    <option selected>Médio</option>
+                    <option selected>Pequeno</option>
+                    </select>
+                </div>   
                 <div class="col">
                      <label class="campo-animal">Idade</label>
                     <input type="text" class="form-control" value="{{ $animal->idade }}">
@@ -33,30 +35,55 @@
                     <label class="campo-animal">Cor</label>
                     <input type="text" class="form-control" value="{{ $animal->cor }}">
                  </div>
-                <div class="col">
+                 <div class="col">
                     <label class="campo-animal">Personalidade</label>
-                    <input type="text" class="form-control" value="{{ $animal->personalidade }}">
-                </div>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" value="{{ $animal->personalidade }}">
+                    <option selected>Calmo</option>
+                    <option selected>Temperamental</option>
+                    <option selected>Estressado</option>
+                    <option selected>Tímido</option>
+                    </select>
+                </div>   
                 <div class="col">
                     <label class="campo-animal">Comorbidade</label>
-                    <input type="text" class="form-control" value="{{ $animal->comorbidade }}">
-                 </div>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example"  value="{{ $animal->comorbidade }}">
+                    <option selected>Sim</option>
+                    <option selected>Não</option>
+                    <option selected>Não sei</option>
+                    </select>
+                </div>   
                  <div class="col">
                     <label class="campo-animal">Espécie</label>
-                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" id="cachorro" name="pet" value="cachorro">
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
                     <option selected>Cachorro</option>
                     <option selected>Gato</option>
                     </select>
                  </div>
                  <div class="col">
                     <label class="campo-animal">Gênero</label>
-                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" id="cachorro" name="pet" value="cachorro">
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
                     <option selected>Fêmia</option>
                     <option selected>Macho</option>
                     </select>
                  </div>
-
-
+                 <div class="col">
+                    <label class="campo-animal">Vacina</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    <option selected>Sim</option>
+                    <option selected>Não</option>
+                    </select>
+                 </div>
+                 <div class="col">
+                    <label class="campo-animal">Castração</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                    <option selected>Sim</option>
+                    <option selected>Não</option>
+                    </select>
+                 </div>
+                 <div class="col">
+                     <label class="campo-animal">Localização</label>
+                    <input type="text" class="form-control" value="{{ $animal->localidade }}">
+                </div>
             </div>
          </div>
 
@@ -71,38 +98,8 @@
                     <label form="foto" >Foto </label>
                     <input id="foto" type="file" name="foto" value="{{ $animal->foto }}"><!--required -->
                 </div> --}}
-
-                <div class="col">
-                <label >Vacina</label>
-                <input type="text" class="form-control" value="{{ $animal->vacina }}">
-            </div>
-
-                <div class="col">
-                    <div class="gender-title">
-                        <h6 class="campo-animal">Castração</h6>
-                    </div>
-
-                    <div class="gender-group">
-                    <div class="gender-input">
-                        <input type="radio" id="sim" name="castracao" value="sim" {{ $animal->castracao == 'sim' ? 'checked' : '' }}>
-                        <label for="sim">Sim</label>
-                    </div>
-
-                    <div class="gender-input">
-                        <input type="radio" id="nao" name="castracao" value="nao" {{ $animal->castracao == 'nao' ? 'checked' : '' }}>
-                        <label for="nao">Não</label>
-                </div>
-
-                <div class="col">
-                <label class="campo-animal">Localização</label>
-                <input type="text" class="form-control" value="{{ $animal->localidade }}">
-            </div>
             <div>
               <button type="submit" class="edit-animal">Atualizar Animal</button>
             </div>
           </form>
-</div>
-                </div>
-
-
 @endsection
