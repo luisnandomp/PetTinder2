2ed14a5587ff2ade2bbdfbf9b6fb6ba7711e2306
marialuizacamilo92@gmail.com <marialuizacamilo92@gmail.com>
2023-06-18 17:49:04 -0300

    modi

--- resources/views/sites/quem.blade.php
+++ resources/views/sites/quem.blade.php
@@ -14,7 +14,7 @@
                 <h2 class="objetivo">
                     Somos alunos da Etec e cursamos Desenvolvimento de Sistemas, decidimos criar este projeto como a nossa conclusão deste curso. <br>
                     A ideia surgiu, pois com o aumento de casos de animais perdidos no município de Guararapes está aumentando a cada dia mais, com isso consequentemente os pets acabam ficando sem lar. Com a aplicação (sistema PET) os usuários têm a possibilidade de encontrar seu pet de forma mais fácil. Além disso existe muitos animais domésticos que estão abandonados nas ruas, que precisam de lar. O sistema viabiliza o auxílio de encontrar uma morada para o pet expondo todas as suas condições.
-                    Com isso o objetivo do sistema é gerenciar e facilitar a adoção e a encontrar seu animal, seja resgatando ou pela adoção responsável. O Sistema possui a opção da interação entre os usuários fazendo postagens e por publicações, com o intuito de facilitar a comunicação entre os usuários.
+                    Com isso o objetivo do sistema é gerenciar e facilitar a adoção e a encontrar seu animal, seja resgatando ou pela adoção responsável. O Sistema possui a opção da interação entre os usuários fazendo  publicações, com o intuito de facilitar a comunicação entre eles.
                 </h2>
             </div>
         </div>
