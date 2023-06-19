eb749ab7f0a7d3b22853e89ba6c09642435aab4d
marialuizacamilo92@gmail.com <marialuizacamilo92@gmail.com>
2023-06-19 17:42:41 -0300

    logo-png

--- resources/views/layout/padrao.blade.php
+++ resources/views/layout/padrao.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" > <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>PetTinder</title>
         <link rel="stylesheet" href="style.css">
-        <link rel="icon" href="{{ asset('imagens/logo_sem_fundo.png') }}" type="image/png">
+        <link rel="icon" href="{{ asset('imagens/logo_fundo.png') }}" type="image/png">
         @vite(['resources/scss/padrao.scss'])
     </head>
     <body>
